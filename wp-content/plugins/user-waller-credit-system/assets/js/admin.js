269b7e407e2e13e9dcb0fcffce2063b7384238ca
Wang Xing <wangstar1031@hotmail.com>
2019-05-18 08:13:36 +0800

    Fixed wallet issue.

--- wp-content/plugins/user-waller-credit-system/assets/js/admin.js
+++ wp-content/plugins/user-waller-credit-system/assets/js/admin.js
@@ -1,11 +1,11 @@
 (function ($) {
 
     /** intiate jQuery tabs */
-    $("#wo_tabs").tabs({
-        activate: function (event, ui) {
-            //window.location.hash = ui.newPanel.attr('id'); // Does not seem to work 100%
-        }
-    });
+    // $("#wo_tabs").tabs({
+    //     activate: function (event, ui) {
+    //         //window.location.hash = ui.newPanel.attr('id'); // Does not seem to work 100%
+    //     }
+    // });
 
     $('#adjust-users-virtual-wallet').submit(function (e) {
         $('#adjust-users-virtual-wallet').children('#submit').hide();
